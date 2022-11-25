4aa6c617e8d75b5abfc66d7e6ba76c7ab32eda19
PCTai <pntai121101@gmail.com>
2022-11-25 15:38:57 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
 import './App.css';
+import { url } from './constants'
 
 function App() {
+  const [advice, setAdvice] = useState();
+
+  const getAdvice = async () => {
+    try {
+      const advice = await fetch(url).then(res => {
+        return res.json()
+      }).then(res => {
+        console.log(res.slip.advice);
+        setAdvice(res.slip.advice);
+        return res.slip.advice;
+      });
+
+    } catch (error) {
+
+    }
+  }
+  useEffect(() => {
+    getAdvice();
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3 className='advice'>{advice}</h3>
+      <button className='btn' onClick={getAdvice}>Get me advice</button>
     </div>
   );
 }
